aefee0adb5e8b7a13e9a740e91f1e36f69ef37d0
Tatiana Priskoka <tatianapriskoka@gmail.com>
2023-01-22 18:53:50 +0300

    feat: js menu

--- plants/index.js
+++ plants/index.js
@@ -1,6 +1,5 @@
-const nav = document.querySelector('.nav-toggle');
-const menu = document.querySelector('header__nav');
-nav.addEventListener('click', () => {
-    console.log(1);
-    menu.classList.toggle('active')
+let nav = document.querySelector('.nav-toggle');
+let menu = document.querySelector('.header__nav');
+nav.addEventListener('click', function () {
+    menu.classList.toggle('active');
 });
